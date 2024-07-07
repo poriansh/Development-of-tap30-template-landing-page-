2211d8f80b21f13c35017e9c979ea661374ba0a5
poria neyshaboriyan <poria.nsh@gmail.com>
2024-07-07 22:37:39 +0330

    feat: section servic"

--- script/script.js
+++ script/script.js
@@ -6,8 +6,22 @@ taggelmenu.addEventListener("click", () => {
   if (navmenu.style.height) {
     navmenu.style.height = null;
     navmenu.classList.remove("header-nav--active");
-    } else {
+  } else {
     navmenu.style.height = navmenu.scrollHeight + "px";
     navmenu.classList.add("header-nav--active");
   }
+});
+
+document.querySelectorAll(".service__item").forEach((item) => {
+  item.addEventListener("click", () => {
+
+    document.querySelector(".service__item--active").classList.remove("service__item--active");
+    item.classList.add("service__item--active");
+    // 
+    document.querySelector(".service-discription--active")
+    .classList.remove("service-discription--active");
+    
+    const tabId = item.getAttribute("data-id");
+    document.getElementById(tabId).classList.add("service-discription--active");
+  });
 });
